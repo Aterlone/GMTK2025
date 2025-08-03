a59c15d25f63e58aff5e3d88fd38da877b52a99f
aterlone <TaneHainesNZ@gmail.com>
2025-08-03 13:22:40 +1200

    Code for music playing is down.

--- UI/escape_menu.gd
+++ UI/escape_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var ominous_once = false
+
 func _ready() -> void:
 	for child in $CanvasLayer/settings.get_children():
 		child.visible = false
@@ -40,3 +42,36 @@ func _on_full_screen_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _process(float) -> void:
+	if ominous_once and not $music/preparation.playing and not $music/ominous_transition.playing:
+		$music/ominous_transition.playing = true
+		ominous_once = true
+	waiting_for_battle()
+
+# When swapping to battle phase wait until enemies come onto screen.
+func waiting_for_battle():
+	if $music/ominous_transition.playing:
+		return false
+	$music/danger_ahead.playing = true
+	$music/danger_ahead.auto_loop = true
+	return true
+
+# When enemies come into screen
+func charge():
+	$music/danger_ahead.playing = false
+	$music/danger_ahead.auto_loop = false
+	$music/charge.playing = true
+
+# Play GReedy Bastard after charge has been played.
+func after_charge():
+	if $music/charge.playing:
+		return false
+	$music/greedy_bastard.playing = true
+	$music/greedy_bastard.auto_loop = true
+	return true
+
+func level_change():
+	$music/greedy_bastard.playing = false
+	$music/greedy_bastard.auto_loop = false
+	ominous_once = false
